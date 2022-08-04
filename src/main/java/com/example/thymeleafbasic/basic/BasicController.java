9b5bcb077b0a728c22d12b3da9e1783813950b4a
kk210001 <kk210001@naver.com>
2022-08-04 21:06:03 +0900

    타임리프 반복기능 <tr th:each"o: ${model.o}"> list, iterable, enum, map 등등 사용가능
    반복 상태 유지 기능
    index:0부터 시작
    count:1부터시작
    size:전체사이즈
    even, odd:홀짝여부
    first.last:첫, 끝 여부
    current:현재객체

--- src/main/java/com/example/thymeleafbasic/basic/BasicController.java
+++ src/main/java/com/example/thymeleafbasic/basic/BasicController.java
@@ -1,6 +1,7 @@
 package com.example.thymeleafbasic.basic;
 
 import lombok.Data;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -89,6 +90,21 @@ public class BasicController {
     public String attribute() {
 
         return "basic/attribute";
+    }
+    @GetMapping("/each")
+    public String each(Model model) {
+
+        addUsers(model);
+        return "basic/each";
+    }
+    
+    private void addUsers(Model model) {
+        List<User> list = new ArrayList<>();
+        list.add(new User("userA",10));
+        list.add(new User("userB",20));
+        list.add(new User("userC",30));
+
+        model.addAttribute("users", list);
     }
 
     @Component("helloBean")
